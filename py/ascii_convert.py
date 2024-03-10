ee2db75cf1bad4c4d6e4b6dd33b62f0f0a303ee8
rift-cpu <mineofshadow@gmail.com>
2024-03-10 23:32:02 +0300

    updated comments

--- py/ascii_convert.py
+++ py/ascii_convert.py
@@ -1,6 +1,8 @@
 from PIL import Image
+ # actually called Pillow when installing, image manipulation import
 
-ASCII_CHARS = ["@", "#", "S", "%", "?", "*", "+", ";", ":", ",", "."]
+ASCII_CHARS = ["@", "#", "S", "%", "?", "*", "+", ";", ":", ",", "."] 
+# symbols based on the level of grayscale in the modified image
 
 def resize_img(image, new_width=100):
     width, height = image.size
@@ -8,15 +10,31 @@ def resize_img(image, new_width=100):
     new_height = int(new_width * ratio)
     resized_image = image.resize((new_width, new_height))
     return(resized_image)
+# resizes image based on the input(100), self-explanatory calculations beyond that
+
 
 def grayscale(image):
     grayscale_image = image.convert("L")
     return(grayscale_image)
-    
+# turns the image into grayscale, L for Luminance
+
 def pixels_to_ascii(image):
     pixels = image.getdata()
     characters = "".join([ASCII_CHARS[pixel//25] for pixel in pixels])
     return(characters)    
+
+# takes the image data as a data tuple (in the case of grayscale, that would look like:
+#53
+#53
+#53
+#53 
+#53
+# each number above is 1 pixel from image.getdata()
+
+# based on the image input, you could end up with a lot of data, the example image used had 160000 pixels in total
+
+# the .join takes all the available pixel data, divided by 25 since we only have 11 "levels" of ascii characters and returns the result as a string
+# this works with every potential level (up to 255) because the result is always an integer
 
 def main(new_width=100):
     path = input("Enter a valid pathname to an image:\n")
